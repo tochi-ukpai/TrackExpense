927a659225683ca9719309316e4f45ea5727c2de
Tochukwu Ukpai <tochukwuukpai@gmail.com>
2023-06-02 19:17:12 +0100

    Initial Commit

---
+++ TrackExpense/TrackExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackExpenseApp.swift
+//  TrackExpense
+//
+//  Created by Theós on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrackExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
